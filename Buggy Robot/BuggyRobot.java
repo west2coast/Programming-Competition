f7ae8f3d27ca8def2d5cbc044db476bff939ff77
west2coast <User@Chriss-MBP-2.hsd1.ga.comcast.net>
2017-09-07 14:20:45 -0400

    Convert String to Char Array

--- Buggy Robot/BuggyRobot.java
+++ Buggy Robot/BuggyRobot.java
@@ -5,17 +5,16 @@ public class BuggyRobot{
 		Scanner input = new Scanner(System.in);
 		int y = 0, x = 0; 
 		String s;
-		
 		y = input.nextInt();
 		x = input.nextInt();
 		char[][] grid = new char[y][x]; // Since I called the array a grid y is vertical and the x is horizontal
 		for (int m = 0; m < y; m++){
 			String value = input.next();
+            char [] temp = value.toCharArray();
 			for (int n = 0; n < x; x++){
-				
-				if(value.matches("U|D|R|L"))
-				grid[m][n] = input.nextChar();
+				grid[m][n] = temp[n];
 			}
+                        
 		}
 
 	}
